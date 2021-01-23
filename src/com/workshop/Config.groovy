fd351b063bd37ae9ad1e4413daef7d6c7eccb3c3
Teddy <tedd50@gmail.com>
2021-01-23 16:15:20 +0700

    prebuild

--- src/com/workshop/Config.groovy
+++ src/com/workshop/Config.groovy
@@ -1,6 +1,12 @@
+
+
 #!/usr/bin/groovy
 package com.workshop
  
 class Config {
-   // TODO: Add Default Variable
+   // Docker related default variable
+   def default_docker_jenkins_tool = 'docker'
+ 
+   // Golang related default variable
+   def default_golang_base_image = "tobapramudia/tkpd-demo:onbuild"
 }
